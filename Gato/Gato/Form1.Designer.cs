5fb89cc469aed06a6bee2f8d5d57e237b1e11792
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2020-06-21 19:03:42 -0600

    Marcar casilla del jugador y usar el servicio para mandar esa información

--- Gato/Gato/Form1.Designer.cs
+++ Gato/Gato/Form1.Designer.cs
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.casilla0 = new System.Windows.Forms.Label();
+            this.casilla1 = new System.Windows.Forms.Label();
+            this.casilla2 = new System.Windows.Forms.Label();
+            this.casilla3 = new System.Windows.Forms.Label();
+            this.casilla4 = new System.Windows.Forms.Label();
+            this.casilla5 = new System.Windows.Forms.Label();
+            this.casilla6 = new System.Windows.Forms.Label();
+            this.casilla7 = new System.Windows.Forms.Label();
+            this.casilla8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.casilla0.AutoSize = true;
+            this.casilla0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla0.Location = new System.Drawing.Point(161, 112);
+            this.casilla0.Name = "0";
+            this.casilla0.Size = new System.Drawing.Size(126, 46);
+            this.casilla0.TabIndex = 0;
+            this.casilla0.Text = "label1";
+            this.casilla0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla0.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label2
+            // 
+            this.casilla1.AutoSize = true;
+            this.casilla1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla1.Location = new System.Drawing.Point(391, 112);
+            this.casilla1.Name = "1";
+            this.casilla1.Size = new System.Drawing.Size(126, 46);
+            this.casilla1.TabIndex = 1;
+            this.casilla1.Text = "label2";
+            this.casilla1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla1.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label3
+            // 
+            this.casilla2.AutoSize = true;
+            this.casilla2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla2.Location = new System.Drawing.Point(606, 112);
+            this.casilla2.Name = "2";
+            this.casilla2.Size = new System.Drawing.Size(126, 46);
+            this.casilla2.TabIndex = 2;
+            this.casilla2.Text = "label3";
+            this.casilla2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla2.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label4
+            // 
+            this.casilla3.AutoSize = true;
+            this.casilla3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla3.Location = new System.Drawing.Point(165, 214);
+            this.casilla3.Name = "3";
+            this.casilla3.Size = new System.Drawing.Size(126, 46);
+            this.casilla3.TabIndex = 3;
+            this.casilla3.Text = "label4";
+            this.casilla3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla3.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label5
+            // 
+            this.casilla4.AutoSize = true;
+            this.casilla4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla4.Location = new System.Drawing.Point(395, 213);
+            this.casilla4.Name = "4";
+            this.casilla4.Size = new System.Drawing.Size(126, 46);
+            this.casilla4.TabIndex = 4;
+            this.casilla4.Text = "label5";
+            this.casilla4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla4.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label6
+            // 
+            this.casilla5.AutoSize = true;
+            this.casilla5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla5.Location = new System.Drawing.Point(610, 212);
+            this.casilla5.Name = "5";
+            this.casilla5.Size = new System.Drawing.Size(126, 46);
+            this.casilla5.TabIndex = 5;
+            this.casilla5.Text = "label6";
+            this.casilla5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla5.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label8
+            // 
+            this.casilla6.AutoSize = true;
+            this.casilla6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla6.Location = new System.Drawing.Point(395, 324);
+            this.casilla6.Name = "7";
+            this.casilla6.Size = new System.Drawing.Size(126, 46);
+            this.casilla6.TabIndex = 7;
+            this.casilla6.Text = "label8";
+            this.casilla6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla6.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label9
+            // 
+            this.casilla7.AutoSize = true;
+            this.casilla7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla7.Location = new System.Drawing.Point(614, 325);
+            this.casilla7.Name = "8";
+            this.casilla7.Size = new System.Drawing.Size(126, 46);
+            this.casilla7.TabIndex = 8;
+            this.casilla7.Text = "label9";
+            this.casilla7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla7.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // label7
+            // 
+            this.casilla8.AutoSize = true;
+            this.casilla8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.casilla8.Location = new System.Drawing.Point(169, 325);
+            this.casilla8.Name = "6";
+            this.casilla8.Size = new System.Drawing.Size(126, 46);
+            this.casilla8.TabIndex = 6;
+            this.casilla8.Text = "label7";
+            this.casilla8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.casilla8.Click += new System.EventHandler(this.eventoMarcarCasilla);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.casilla7);
+            this.Controls.Add(this.casilla6);
+            this.Controls.Add(this.casilla8);
+            this.Controls.Add(this.casilla5);
+            this.Controls.Add(this.casilla4);
+            this.Controls.Add(this.casilla3);
+            this.Controls.Add(this.casilla2);
+            this.Controls.Add(this.casilla1);
+            this.Controls.Add(this.casilla0);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label casilla0;
+        private System.Windows.Forms.Label casilla1;
+        private System.Windows.Forms.Label casilla2;
+        private System.Windows.Forms.Label casilla3;
+        private System.Windows.Forms.Label casilla4;
+        private System.Windows.Forms.Label casilla5;
+        private System.Windows.Forms.Label casilla6;
+        private System.Windows.Forms.Label casilla7;
+        private System.Windows.Forms.Label casilla8;
     }
 }
 
--- Gato/Gato/Form1.cs
+++ Gato/Gato/Form1.cs
@@ -12,9 +12,66 @@ namespace Gato
 {
     public partial class Form1 : Form
     {
+        private ServicioGatoJose.TikTakToePortClient gato;
+        private string tablero;
         public Form1()
         {
+            gato = new ServicioGatoJose.TikTakToePortClient();
+            tablero = "_________";
+            Console.Out.WriteLine(tablero);
             InitializeComponent();
         }
+
+        private void eventoMarcarCasilla(object sender, EventArgs e)
+        {
+            Label casilla = (Label)sender;
+
+            // No marcar una que ya este marcada
+            if(casilla.Text != "X" && casilla.Text != "O")
+            {
+                casilla.Text = "X";
+                casilla.TextAlign = ContentAlignment.MiddleCenter;
+
+                Coordenada coordenada = convertirCoordenada(int.Parse(casilla.Name));
+                marcarCasilla(coordenada);
+            }
+        }
+
+        private void marcarCasilla(Coordenada coordenada)
+        {
+            tablero = gato.turno(tablero, coordenada.x, coordenada.y, "X");
+            marcarCasillaOponente();
+            
+        }
+
+        private Coordenada convertirCoordenada(int posicionCasilla)
+        {
+            switch(posicionCasilla)
+            {
+                case 0: return new Coordenada(0, 0);
+                case 1: return new Coordenada(0, 1);
+                case 2: return new Coordenada(0, 2);
+                case 3: return new Coordenada(1, 0);
+                case 4: return new Coordenada(1, 1);
+                case 5: return new Coordenada(1, 2);
+                case 6: return new Coordenada(2, 0);
+                case 7: return new Coordenada(2, 1);
+                case 8: return new Coordenada(2, 2);
+                default: return null;
+            }
+        }
+
+        private class Coordenada
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            public Coordenada(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
     }
+
 }
